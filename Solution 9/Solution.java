483917ffc59a80749a83ab14a8c1dc93f2403a0a
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-07-18 16:45:21 +0300

    9. Palindrome Number

---
+++ Solution 9/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+}
