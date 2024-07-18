08b4ca2d93478c15d3504e57ada957f831d46fa6
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-07-18 15:11:00 +0300

    121. Best Time to Buy and Sell Stock

---
+++ Top Interview 150/Solution121/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        
+        return maxProfit;
+    }
+}
