57a1bacc446290bd9af6279da7f18280a1724fba
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-07-26 16:51:36 +0300

    66. Plus One

---
+++ Top Interview 150/Solution65/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            
+            digits[i] = 0;
+        }
+        
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1; 
+        return newDigits;
+    }
+}
