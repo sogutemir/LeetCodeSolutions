f8e6be6c794f6aa43265952457099f6036679d13
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-07-30 14:40:38 +0300

    28. Find the Index of the First Occurrence in a String

---
+++ Top Interview 150/Solution28/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        
+        int haystackLength = haystack.length();
+        int needleLength = needle.length();
+        
+        if (needleLength > haystackLength) {
+            return -1;
+        }
+        
+        for (int i = 0; i <= haystackLength - needleLength; i++) {
+            if (haystack.substring(i, i + needleLength).equals(needle)) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
