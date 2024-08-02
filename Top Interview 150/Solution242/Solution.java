1f2ce880361bd537d651b7861b5617dc72b0f1ec
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-02 22:28:41 +0300

    242. Valid Anagram

---
+++ Top Interview 150/Solution242/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        int[] count = new int[26];  // alphabet character counts
+        
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
