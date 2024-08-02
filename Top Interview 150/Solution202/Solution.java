9163c7cc1494c44bb2b62c2c84d6472a413361ce
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-02 18:38:27 +0300

    202. Happy Number

---
+++ Top Interview 150/Solution202/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getSumOfSquares(n);
+        }
+        
+        return n == 1;
+    }
+    
+    private int getSumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
