b8180553adade5cf6386e80b237cd9ec73c98072
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-02 17:34:30 +0300

    191. Number of 1 Bits

---
+++ Top Interview 150/Solution191/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        
+        while (n != 0) {
+            count += (n & 1); 
+            n >>= 1; 
+        }
+        
+        return count;
+    }
+}
