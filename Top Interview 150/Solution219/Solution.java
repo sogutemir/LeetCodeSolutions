00d5e7102ce69a0cf02ed554d8dbcbfd5778e997
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-02 20:21:54 +0300

    219. Contains Duplicate II

---
+++ Top Interview 150/Solution219/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int prevIndex = map.get(nums[i]);
+                if (i - prevIndex <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}
