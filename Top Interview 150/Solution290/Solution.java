4098ead2b7764fd4991f5eddde93cb10af34e958
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-02 22:17:24 +0300

    290. Word Pattern

---
+++ Top Interview 150/Solution290/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+        
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+        
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+            
+            if (charToWord.containsKey(c)) {
+                if (!charToWord.get(c).equals(word)) {
+                    return false;
+                }
+            } else {
+                charToWord.put(c, word);
+            }
+            
+            if (wordToChar.containsKey(word)) {
+                if (wordToChar.get(word) != c) {
+                    return false;
+                }
+            } else {
+                wordToChar.put(word, c);
+            }
+        }
+        
+        return true;
+    }
+}
