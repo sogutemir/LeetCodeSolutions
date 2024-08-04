598336ec181a71538bcd0c1febde1ec4e39588f6
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-04 16:14:24 +0300

    1. Two Sum

---
+++ Solution1/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (numToIndex.containsKey(complement)) {
+                return new int[] { numToIndex.get(complement), i };
+            }
+            
+            numToIndex.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
