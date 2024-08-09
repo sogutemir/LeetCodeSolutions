0138a1fcc5e1b8bc03c31a199b3b2099657f587e
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-10 02:29:23 +0300

    115. Distinct Subsequences

---
+++ Solution115/Solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.numDistinct("rabbbit", "rabbit"));
+        System.out.println(sol.numDistinct("babgbag", "bag"));
+    }
+}
