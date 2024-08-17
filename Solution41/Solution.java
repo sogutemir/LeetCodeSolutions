ff9ef178f8dda623fafbec83cb3365a4725571a3
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-17 16:50:12 +0300

    41. First Missing Positive

---
+++ Solution41/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+}
