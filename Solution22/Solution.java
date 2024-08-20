82c3f4aa07f7b888ec2234e437318d1aac267cfb
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-20 16:08:00 +0300

    22. Generate Parentheses

---
+++ Solution22/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, String current, int open, int close, int max) {
+        if (current.length() == max * 2) {
+            result.add(current);
+            return;
+        }
+
+        if (open < max) {
+            backtrack(result, current + "(", open + 1, close, max);
+        }
+
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, max);
+        }
+    }
+}
