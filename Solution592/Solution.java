24f93201ee6e067da66941569f614f4939abce29
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-23 17:35:40 +0300

    Code Testcase Testcase Test Result 592. Fraction Addition and Subtraction

---
+++ Solution592/Solution.java
@@ -0,0 +1,52 @@
+class Solution {
+    public String fractionAddition(String expression) {
+
+        int numerator = 0;
+        int denominator = 1;
+        
+        int i = 0;
+        while (i < expression.length()) {
+
+            int sign = 1;
+            if (expression.charAt(i) == '+' || expression.charAt(i) == '-') {
+                sign = expression.charAt(i) == '-' ? -1 : 1;
+                i++;
+            }
+            
+            int num = 0;
+            while (i < expression.length() && Character.isDigit(expression.charAt(i))) {
+                num = num * 10 + (expression.charAt(i) - '0');
+                i++;
+            }
+            num *= sign;
+            
+            i++;
+            
+            int denom = 0;
+            while (i < expression.length() && Character.isDigit(expression.charAt(i))) {
+                denom = denom * 10 + (expression.charAt(i) - '0');
+                i++;
+            }
+            
+            numerator = numerator * denom + num * denominator;
+            denominator *= denom;
+            
+            int gcd = gcd(Math.abs(numerator), Math.abs(denominator));
+            numerator /= gcd;
+            denominator /= gcd;
+        }
+        
+        if (denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+        
+        return numerator + "/" + denominator;
+    }
+    
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+}
